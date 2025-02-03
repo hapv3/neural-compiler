hapv3/neural-compiler

commit af541e72a5931dda691e86510772a5da6d549a57

MLBEDSW-10168: Zero point handling pass
- Legalize zero point setting for TFLite according to reference
  implementation
- Move logic away from RCS

Change-Id: I7659f438c6df7b3197f36a00287da6457ac6edd6
Signed-off-by: Mauricio Briceno <mauricio.briceno@arm.com>

ethosu/regor/compiler/tflite_graph_optimiser.cpp
@@ -2758,6 +2758,31 @@ Operation *TFLiteGraphOptimiser::ConvertPad(Graph *const graph, Operation *const
     return mainOp.get();
 }
 
+Operation *TFLiteGraphOptimiser::ConvertZeroPoint(Graph *const graph, Operation *const operation)
+{
+    UNUSED(graph);
+    auto opType = operation->Type();
+    bool zeroPoint0ForType =
+        opType == OpType::AvgPool || opType == OpType::Resize || opType == OpType::CLZ || opType == OpType::SHL || opType == OpType::Div;
+
+    for ( auto [usage, ifmConn] : operation->Inputs().pairs() )
+    {
+        if ( IsIFM(usage) )
+        {
+            if ( zeroPoint0ForType || DataTypeSizeBits(ifmConn.tensor->Type()) >= 32 )
+                ifmConn.quantization.zeroPoints.clear();
+        }
+    }
+    for ( auto [usage, ofmConn] : operation->Outputs().pairs() )
+    {
+        if ( IsOFM(usage) )
+        {
+            if ( zeroPoint0ForType || opType == OpType::ArgMax ) ofmConn.quantization.zeroPoints.clear();
+        }
+    }
+    return operation;
+}
+
 TFLiteGraphOptimiser::TFLiteGraphOptimiser(IArchitectureConstraints *constraints, const GraphOptimiserOptions &options, OptimiserDatabase *db) :
         GraphOptimiser(constraints, options, db)
 {

ethosu/regor/architecture/ethos_u_scaling.cpp
@@ -82,7 +82,6 @@ Quantization RescalePerChannel(const Quantization &ifmQuant, const Quantization 
     quantResult.quantMin = ofmQuant.quantMin;
     quantResult.quantMax = ofmQuant.quantMax;
     quantResult.dimension = ofmQuant.dimension;
-    quantResult.forceZeroPoint = ofmQuant.forceZeroPoint;
 
     if ( !ifmQuant.scales.empty() && !ofmQuant.scales.empty() && !weightQuant.scales.empty() )
     {

ethosu/regor/architecture/ethosu55/ethos_u55_register_cs_generator.cpp
@@ -427,19 +427,6 @@ uint32_t EthosU55RCSGenerator::ToRegion(const MemArea &memArea)
     return uint32_t(region);
 }
 
-bool EthosU55RCSGenerator::UseZeroPoint0(OpType opType, const HLCFeatureMap &fm, bool isOFM)
-{
-    if ( fm.quantization.forceZeroPoint )
-    {
-        return false;
-    }
-    if ( fm.quantization.zeroPoints.empty() || (fm.dataType == DataType::Int32 && !isOFM) )
-    {
-        return true;
-    }
-    return opType == OpType::AvgPool || opType == OpType::ResizeBilinear || opType == OpType::CLZ || opType == OpType::SHL;
-}
-
 // Checks if the feature map is a scalar, and if so, returns the
 // quantized value in scalarValue.
 bool EthosU55RCSGenerator::IsScalar(const HLCFeatureMap &fm, int32_t &scalarValue)
@@ -969,7 +956,7 @@ void EthosU55RCSGenerator::GenerateIFM(OpType opType, const HLCFeatureMap &fm, c
     Emit(isa::npu_set_ifm_stride_c_t(strides.Depth()));
     // IFM_ZERO_POINT register
     auto &quant = fm.quantization;
-    uint32_t zp = UseZeroPoint0(opType, fm, false) ? 0 : uint32_t(quant.zeroPoints[0]);
+    uint32_t zp = quant.zeroPoints.empty() ? 0 : uint32_t(quant.zeroPoints[0]);
     Emit(isa::npu_set_ifm_zero_point_t(zp));
 }
 
@@ -1002,7 +989,7 @@ void EthosU55RCSGenerator::GenerateIFM2(OpType opType, const HLCFeatureMap &fm, 
     }
     // IFM2_ZERO_POINT register
     auto &quant = fm.quantization;
-    uint32_t zp = UseZeroPoint0(opType, fm, false) ? 0 : uint32_t(quant.zeroPoints[0]);
+    uint32_t zp = quant.zeroPoints.empty() ? 0 : uint32_t(quant.zeroPoints[0]);
     Emit(isa::npu_set_ifm2_zero_point_t(zp));
 }
 
@@ -1033,7 +1020,7 @@ void EthosU55RCSGenerator::GenerateOFM(OpType opType, const HLCFeatureMap &fm, c
     Emit(isa::npu_set_ofm_stride_c_t(strides.Depth()));
     // OFM_ZERO_POINT register
     auto &quant = fm.quantization;
-    uint32_t zp = UseZeroPoint0(opType, fm, true) ? 0 : uint32_t(quant.zeroPoints[0]);
+    uint32_t zp = quant.zeroPoints.empty() ? 0 : uint32_t(quant.zeroPoints[0]);
     Emit(isa::npu_set_ofm_zero_point_t(zp));
 }
 

ethosu/regor/architecture/ethosu55/ethos_u55_register_cs_generator.hpp
@@ -151,7 +151,6 @@ protected:
     MemoryAccess ToMemoryAccess(const HLCFeatureMap &fm, const Box &area, AccessDirection direction);
     // Returns region number used in NPU_SET_..._REGION
     uint32_t ToRegion(const MemArea &memArea);
-    static bool UseZeroPoint0(OpType opType, const HLCFeatureMap &fm, bool isOFM);
     // Checks if the feature map is a scalar, and if so, returns the
     // quantized value in scalarValue.
     static bool IsScalar(const HLCFeatureMap &fm, int32_t &scalarValue);

ethosu/regor/architecture/ethosu85/ethos_u85_register_cs_generator.cpp
@@ -562,24 +562,6 @@ uint32_t EthosU85RCSGenerator::ToRegion(const MemArea &memArea)
     return uint32_t(region);
 }
 
-bool EthosU85RCSGenerator::UseZeroPoint0(OpType opType, const HLCFeatureMap &fm, bool isOFM)
-{
-    if ( fm.quantization.forceZeroPoint )
-    {
-        return false;
-    }
-    if ( fm.quantization.zeroPoints.empty() || (DataTypeSizeBits(fm.dataType) >= 32 && !isOFM) )
-    {
-        return true;
-    }
-    if ( opType == OpType::ArgMax && isOFM )
-    {
-        return true;
-    }
-    return opType == OpType::AvgPool || opType == OpType::Resize || opType == OpType::CLZ || opType == OpType::SHL || opType == OpType::Div;
-}
-
-
 // Checks if the feature map is a scalar, and if so, returns the
 // quantized value in scalarValue.
 bool EthosU85RCSGenerator::IsScalar(const HLCFeatureMap &fm, int32_t &scalarValue)
@@ -1228,7 +1210,7 @@ void EthosU85RCSGenerator::GenerateIFM(OpType opType, const HLCFeatureMap &fm, c
     }
     // IFM_ZERO_POINT register
     auto &quant = fm.quantization;
-    uint32_t zp = UseZeroPoint0(opType, fm, false) ? 0 : uint32_t(quant.zeroPoints[0]);
+    uint32_t zp = quant.zeroPoints.empty() ? 0 : uint32_t(quant.zeroPoints[0]);
 
     // ifm zero-point is force-emitted if ofm is chained
     Emit(isa::npu_set_ifm_zero_point_t(zp));
@@ -1272,7 +1254,7 @@ void EthosU85RCSGenerator::GenerateIFM2(
     }
     // IFM2_ZERO_POINT register
     auto &quant = fm.quantization;
-    uint32_t zp = UseZeroPoint0(opType, fm, false) ? 0 : uint32_t(quant.zeroPoints[0]);
+    uint32_t zp = quant.zeroPoints.empty() ? 0 : uint32_t(quant.zeroPoints[0]);
 
     // ifm zero-point is force-emitted if ofm is chained
     Emit(isa::npu_set_ifm2_zero_point_t(zp));
@@ -1316,7 +1298,7 @@ void EthosU85RCSGenerator::GenerateOFM(OpType opType, const HLCFeatureMap &fm, c
     }
     // OFM_ZERO_POINT register
     auto &quant = fm.quantization;
-    uint32_t zp = UseZeroPoint0(opType, fm, true) ? 0 : uint32_t(quant.zeroPoints[0]);
+    uint32_t zp = quant.zeroPoints.empty() ? 0 : uint32_t(quant.zeroPoints[0]);
     Emit(isa::npu_set_ofm_zero_point_t(zp));
 }
 

ethosu/regor/architecture/ethosu85/ethos_u85_register_cs_generator.hpp
@@ -135,7 +135,6 @@ protected:
     MemoryAccess ToMemoryAccess(const HLCFeatureMap &fm, const Box &area, AccessDirection direction);
     // Returns region number used in NPU_SET_..._REGION
     uint32_t ToRegion(const MemArea &memArea);
-    static bool UseZeroPoint0(OpType opType, const HLCFeatureMap &fm, bool isOFM);
     // Checks if the feature map is a scalar, and if so, returns the
     // quantized value in scalarValue.
     static bool IsScalar(const HLCFeatureMap &fm, int32_t &scalarValue);

ethosu/regor/compiler/graph_builder.cpp
@@ -540,7 +540,6 @@ void GraphBuilder::SetZeroPoint(GraphOperation *graphOp, GraphTensorUsage tensor
     if ( conn )
     {
         conn->quantization.zeroPoints = {int64_t(zeroPoint)};
-        conn->quantization.forceZeroPoint = true;
     }
 }
 

ethosu/regor/compiler/quantization.cpp
@@ -30,14 +30,14 @@ std::string Quantization::ToString() const
     {
         scale.push_back(fmt::format("(scale:{}, shift:{})", s.scale, s.shift));
     }
-    return fmt::format("scale: [{}], zero_point: [{}], quantMin: [{}], quantMax: [{}], dimension: {}, force_zero_point: {}",
-        fmt::join(scale, ", "), fmt::join(zeroPoints, ", "), fmt::join(quantMin, ", "), fmt::join(quantMax, ", "), dimension, forceZeroPoint);
+    return fmt::format("scale: [{}], zero_point: [{}], quantMin: [{}], quantMax: [{}], dimension: {}",
+        fmt::join(scale, ", "), fmt::join(zeroPoints, ", "), fmt::join(quantMin, ", "), fmt::join(quantMax, ", "), dimension);
 }
 
 bool Quantization::operator==(const Quantization &rhs) const
 {
-    return std::tie(scales, zeroPoints, quantMin, quantMax, dimension, forceZeroPoint) ==
-           std::tie(rhs.scales, rhs.zeroPoints, rhs.quantMin, rhs.quantMax, rhs.dimension, rhs.forceZeroPoint);
+    return std::tie(scales, zeroPoints, quantMin, quantMax, dimension) ==
+           std::tie(rhs.scales, rhs.zeroPoints, rhs.quantMin, rhs.quantMax, rhs.dimension);
 }
 
 bool Quantization::operator!=(const Quantization &rhs) const

commit 3f7624d370701c2aae493860b86adc238711d50a

MLBEDSW-10364: Don't run ConvertZeroPoint for Passthrough ops
This is a regression since af541e72a.

Signed-off-by: Johan Gunnarsson <johan.gunnarsson@arm.com>
Change-Id: I53a5a555d738a8e2527a70e0c5f2ad7dcb6baf29

ethosu/regor/compiler/tflite_graph_optimiser.cpp
@@ -2797,9 +2797,13 @@ Operation *TFLiteGraphOptimiser::ConvertZeroPoint(Graph *const graph, Operation 
 {
     UNUSED(graph);
     auto opType = operation->Type();
+    if ( opType == OpType::Passthrough )
+    {
+        return operation;
+    }
+
     bool zeroPoint0ForType =
         opType == OpType::AvgPool || opType == OpType::Resize || opType == OpType::CLZ || opType == OpType::SHL || opType == OpType::Div;
-
     for ( auto [usage, ifmConn] : operation->Inputs().pairs() )
     {
         if ( IsIFM(usage) )