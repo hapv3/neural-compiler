hapv3/neural-compiler

commit 1defc46a5488c79ca0a93185becce7865cdf9c2b

MLBEDSW-11032: Maintain correct producers vector when fusing
Need to maintain correct producers/consumers, otherwise
UpdateSchedulerTensor might make decisions, such as setting tensor
format, based on wrong information.

Signed-off-by: Johan Gunnarsson <johan.gunnarsson@arm.com>
Change-Id: Ic132379c2761c05272f08255067f0395e9f459f6

ethosu/regor/compiler/scheduler_packing.cpp
@@ -407,7 +407,9 @@ void SchedulerPacking::PackOperations()
                 if ( IsActivation(nextOp->Type()) )
                 {
                     auto *ofmConn = lastNonFusedOp->OFM();
+                    ofmConn->tensor->RemoveWriter(lastNonFusedOp);
                     ofmConn->tensor = nextOp->OFM()->tensor;
+                    ofmConn->tensor->producers.push_back(lastNonFusedOp);
                     ofmConn->SetType(nextOp->OFM()->Type());
                     ofmConn->quantization.quantMin = nextOp->Output(TensorUsage::OFM)->quantization.quantMin;
                     ofmConn->quantization.quantMax = nextOp->Output(TensorUsage::OFM)->quantization.quantMax;
@@ -415,7 +417,9 @@ void SchedulerPacking::PackOperations()
                 else if ( nextOp->Type() == OpType::Transpose )
                 {
                     auto *ofmConn = lastNonFusedOp->OFM();
+                    ofmConn->tensor->RemoveWriter(lastNonFusedOp);
                     ofmConn->tensor = nextOp->OFM()->tensor;
+                    ofmConn->tensor->producers.push_back(lastNonFusedOp);
                     ofmConn->SetType(nextOp->OFM()->Type());
                     ofmConn->shape = nextOp->OFM()->shape;
                     ofmConn->slice = nextOp->OFM()->slice;
@@ -424,7 +428,9 @@ void SchedulerPacking::PackOperations()
                 else if ( nextOp->Type() == OpType::Reverse )
                 {
                     auto *ofmConn = lastNonFusedOp->OFM();
+                    ofmConn->tensor->RemoveWriter(lastNonFusedOp);
                     ofmConn->tensor = nextOp->OFM()->tensor;
+                    ofmConn->tensor->producers.push_back(lastNonFusedOp);
                     ofmConn->SetType(nextOp->OFM()->Type());
                     ofmConn->shape = nextOp->OFM()->shape;
                     ofmConn->slice = nextOp->OFM()->slice;

ethosu/regor/test/test_scheduler_packing.cpp
@@ -119,4 +119,56 @@ TEST_CASE("test_scheduler_packing")
             REQUIRE(axis == ofmShape.Size() - 2);
         }
     }
+
+    SECTION("Pack operation and activation")
+    {
+        std::vector<std::shared_ptr<Operation>> ops;
+        auto ifm = CreateTensor("IFM", Shape(10, 10, 10), DataType::Int8);
+        auto ofm = CreateTensor("OFM", Shape(10, 10, 10), DataType::Int8);
+        auto actofm = CreateTensor("ACTOFM", Shape(10, 10, 10), DataType::Int8);
+        auto op1 = CreateOperation(OpType::Abs, TensorUsage::IFM, ifm, TensorUsage::OFM, ofm);
+        ops.push_back(std::move(op1));
+        auto op2 = CreateOperation(OpType::Relu, TensorUsage::IFM, ofm, TensorUsage::OFM, actofm);
+        ops.push_back(std::move(op2));
+
+        // Create graph with ops
+        auto graph = CreateGraph(ops);
+
+        // Perform scheduler_packing
+        auto schedOps = packing.Process(graph.get());
+        REQUIRE(schedOps.size() == 1);
+
+        // Validate that the second op is packed as subop of first
+        auto &abs = schedOps[0];
+        REQUIRE(abs->Type() == OpType::Abs);
+        REQUIRE(abs->SubOps().size() == 1);
+        auto &relu = abs->SubOps()[0];
+        REQUIRE(relu->Type() == OpType::Relu);
+
+        // Validate the primary op
+        auto *absIfmConn = abs->Input(TensorUsage::IFM);
+        auto *absOfmConn = abs->Output(TensorUsage::OFM);
+        REQUIRE(absIfmConn->tensor->Name() == "IFM");
+        REQUIRE(absIfmConn->tensor->producers.empty());
+        REQUIRE(absIfmConn->tensor->consumers.size() == 1);
+        REQUIRE(absIfmConn->tensor->consumers[0] == abs.get());
+        REQUIRE(absOfmConn->tensor->Name() == "ACTOFM");
+        REQUIRE(absOfmConn->tensor->producers.size() == 2);
+        REQUIRE(absOfmConn->tensor->producers[0] == relu.get());
+        REQUIRE(absOfmConn->tensor->producers[1] == abs.get());
+        REQUIRE(absOfmConn->tensor->consumers.empty());
+
+        // Validate the activation op
+        auto *reluIfmConn = relu->Input(TensorUsage::IFM);
+        auto *reluOfmConn = relu->Output(TensorUsage::OFM);
+        REQUIRE(reluIfmConn->tensor->Name() == "OFM");
+        REQUIRE(reluIfmConn->tensor->producers.empty());
+        REQUIRE(reluIfmConn->tensor->consumers.size() == 1);
+        REQUIRE(reluIfmConn->tensor->consumers[0] == relu.get());
+        REQUIRE(reluOfmConn->tensor->Name() == "ACTOFM");
+        REQUIRE(reluOfmConn->tensor->producers.size() == 2);
+        REQUIRE(reluOfmConn->tensor->producers[0] == relu.get());
+        REQUIRE(reluOfmConn->tensor->producers[1] == abs.get());
+        REQUIRE(reluOfmConn->tensor->consumers.empty());
+    }
 }

commit b1fd444b05562c33753d295aa183294550daeea9

MLBEDSW-11056: Fix performance regression
This fixes a performance regression caused by 1defc46a54.

This changes so the primary op's new OFM (fused op's OFM) only has
1 producer instead of 2. The activation is still connected, but we
don't count is as a producer since it's now the primary op that is
writing to the OFM.

Signed-off-by: Johan Gunnarsson <johan.gunnarsson@arm.com>
Change-Id: I5c4b4051ef709e4bb718e4dde745640300ee158e

ethosu/regor/compiler/scheduler_packing.cpp
@@ -403,13 +403,34 @@ void SchedulerPacking::PackOperations()
                 LOG_TRACE1("Added {} (key {}) to {} (key {})\n", OpTypeToString(nextOp->Type()), key,
                     OpTypeToString(prevOp->Type()), prevOpKey);
 
-                // Replace the last non-fused op's OFM by nextOp's OFM if nextOp is fused
+                // Replace the last non-fused op's OFM by nextOp's OFM if nextOp is fused. Also adjust producers and
+                // consumers of the tensors involved like below.
+                //
+                // Before: [T1] -> PRIMARY -> [T2] -> ACTIVATION -> [T3]
+                // T1 producers = ?
+                // T1 consumers = {PRIMARY}
+                // T2 producers = {PRIMARY}
+                // T2 consumers = {ACTIVATION}
+                // T3 producers = {ACTIVATION}
+                // T3 consumers = ?
+                //
+                // After: [T1] -> PRIMARY (with ACTIVATION as subop) -> [T3]
+                // T1 producers = ?
+                // T1 consumers = {PRIMARY}
+                // T2 producers = {}
+                // T2 consumers = {ACTIVATION}
+                // T3 producers = {PRIMARY}
+                // T3 consumers = ?
+                //
+                // After fuse T2 still exists, but only as an unused input to ACTIVATION.
+
                 if ( IsActivation(nextOp->Type()) )
                 {
                     auto *ofmConn = lastNonFusedOp->OFM();
-                    ofmConn->tensor->RemoveWriter(lastNonFusedOp);
+                    ofmConn->tensor->RemoveWriter(lastNonFusedOp);  // Remove PRIMARY from T2 producers
                     ofmConn->tensor = nextOp->OFM()->tensor;
-                    ofmConn->tensor->producers.push_back(lastNonFusedOp);
+                    ofmConn->tensor->producers.push_back(lastNonFusedOp);  // Add PRIMARY to T3 producers
+                    ofmConn->tensor->RemoveWriter(nextOp);                 // Remove ACTIVATION from T3 producers
                     ofmConn->SetType(nextOp->OFM()->Type());
                     ofmConn->quantization.quantMin = nextOp->Output(TensorUsage::OFM)->quantization.quantMin;
                     ofmConn->quantization.quantMax = nextOp->Output(TensorUsage::OFM)->quantization.quantMax;
@@ -420,6 +441,7 @@ void SchedulerPacking::PackOperations()
                     ofmConn->tensor->RemoveWriter(lastNonFusedOp);
                     ofmConn->tensor = nextOp->OFM()->tensor;
                     ofmConn->tensor->producers.push_back(lastNonFusedOp);
+                    ofmConn->tensor->RemoveWriter(nextOp);
                     ofmConn->SetType(nextOp->OFM()->Type());
                     ofmConn->shape = nextOp->OFM()->shape;
                     ofmConn->slice = nextOp->OFM()->slice;
@@ -431,6 +453,7 @@ void SchedulerPacking::PackOperations()
                     ofmConn->tensor->RemoveWriter(lastNonFusedOp);
                     ofmConn->tensor = nextOp->OFM()->tensor;
                     ofmConn->tensor->producers.push_back(lastNonFusedOp);
+                    ofmConn->tensor->RemoveWriter(nextOp);
                     ofmConn->SetType(nextOp->OFM()->Type());
                     ofmConn->shape = nextOp->OFM()->shape;
                     ofmConn->slice = nextOp->OFM()->slice;

ethosu/regor/test/test_scheduler_packing.cpp
@@ -153,9 +153,8 @@ TEST_CASE("test_scheduler_packing")
         REQUIRE(absIfmConn->tensor->consumers.size() == 1);
         REQUIRE(absIfmConn->tensor->consumers[0] == abs.get());
         REQUIRE(absOfmConn->tensor->Name() == "ACTOFM");
-        REQUIRE(absOfmConn->tensor->producers.size() == 2);
-        REQUIRE(absOfmConn->tensor->producers[0] == relu.get());
-        REQUIRE(absOfmConn->tensor->producers[1] == abs.get());
+        REQUIRE(absOfmConn->tensor->producers.size() == 1);
+        REQUIRE(absOfmConn->tensor->producers[0] == abs.get());
         REQUIRE(absOfmConn->tensor->consumers.empty());
 
         // Validate the activation op
@@ -166,9 +165,8 @@ TEST_CASE("test_scheduler_packing")
         REQUIRE(reluIfmConn->tensor->consumers.size() == 1);
         REQUIRE(reluIfmConn->tensor->consumers[0] == relu.get());
         REQUIRE(reluOfmConn->tensor->Name() == "ACTOFM");
-        REQUIRE(reluOfmConn->tensor->producers.size() == 2);
-        REQUIRE(reluOfmConn->tensor->producers[0] == relu.get());
-        REQUIRE(reluOfmConn->tensor->producers[1] == abs.get());
+        REQUIRE(reluOfmConn->tensor->producers.size() == 1);
+        REQUIRE(reluOfmConn->tensor->producers[0] == abs.get());
         REQUIRE(reluOfmConn->tensor->consumers.empty());
     }
 }